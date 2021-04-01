5394631ea840b23c4eeb9bf1bf77346b4749d3cd
jaebungs <jaebungs@gmail.com>
2021-04-01 17:35:08 -0400

    create clock challenge.

---
+++ ticking-time/app.js
@@ -0,0 +1,19 @@
+const clockContainerEl = document.querySelector('#clock');
+const clockEl = document.createElement('div');
+// display current hours, minutes and seconds.
+
+
+function second() {
+    setTimeout(()=>{
+        clockDisplay()
+    }, 1000)
+}
+
+function clockDisplay() {
+    const time = new Date();
+    clockEl.textContent = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+    clockContainerEl.appendChild(clockEl)
+    second()
+}
+
+clockDisplay()
