49873c4c02b146fbfbe5ebe726bbb83f607adc73
jaebungs <jaebungs@gmail.com>
2021-04-02 19:48:49 -0400

    api call five-friends

--- five-friends/app.js
+++ five-friends/app.js
@@ -1,1 +1,19 @@
 // use api.
+
+const displayEl = document.querySelector('#display');
+const btnEl = document.querySelector('#btn');
+
+async function apiCall (){
+   await fetch('https://randomuser.me/api/?result=5')
+            .then((res)=>res.json())
+            .then((data)=>{
+                console.log(data.results[0].name.first);
+                const personEl = document.createElement('span');
+                personEl.textContent = data.results[0].name.first
+                displayEl.appendChild(personEl)
+            });
+}
+
+btnEl.addEventListener('click', ()=>{
+    apiCall();
+})
